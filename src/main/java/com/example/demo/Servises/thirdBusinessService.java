d8c8fddf1cb825909aed40a82d00749c8f40beaa
rogoz <rogozhin-04@inbox.ru>
2024-07-17 12:51:04 +0300

    Fix unnecessary Query's at thirdBusinessQuery

--- src/main/java/com/example/demo/Servises/thirdBusinessService.java
+++ src/main/java/com/example/demo/Servises/thirdBusinessService.java
@@ -1,12 +1,12 @@
 package com.example.demo.Servises;
 
 import com.example.demo.DTOs.LessonMarksDTO;
-import com.example.demo.Domain.LessonMarks;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.TypedQuery;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -14,25 +14,21 @@ public class thirdBusinessService {
     @PersistenceContext
     protected EntityManager entityManager;
 
-    private List<LessonMarks> firstSubquery(Integer lessonId) {
-        String jpql = "SELECT l FROM LessonMarks l JOIN l.user u WHERE l.lesson.id = :lessonId " +
+    private List<Object[]> firstSubquery(Integer lessonId) {
+        String jpql = "SELECT u.level, l.timeOfMaking, l.mark, l.dateOfMaking FROM LessonMarks l JOIN l.user u WHERE l.lesson.id = :lessonId " +
                 "ORDER BY u.level DESC, l.timeOfMaking, l.dateOfMaking DESC";
-
-        TypedQuery<LessonMarks> query = entityManager.createQuery(jpql, LessonMarks.class);
+        TypedQuery<Object[]> query = entityManager.createQuery(jpql, Object[].class);
         query.setParameter("lessonId", lessonId);
-        return List.copyOf(query.getResultList());
+        return query.getResultList();
     }
 
-//  TODO фикс большого числа запросов, нужно как-то получать инфйу от юзера пармо в запросе (точнее это нужно как-то нормально мапить...)
     public List<LessonMarksDTO> thirdBusinessQuery(Integer lessonId) {
-        List<LessonMarks> lessonMarks = firstSubquery(lessonId);
+        List<Object[]> lessonMarks = firstSubquery(lessonId);
         List<LessonMarksDTO> lessonMarksDTOs = new java.util.ArrayList<>(List.of());
 
-        for (LessonMarks lessonMark : lessonMarks) {
-            lessonMarksDTOs.add(new LessonMarksDTO(lessonMark.getUser().getLevel(), lessonMark.getTimeOfMaking(),
-                    lessonMark.getMark(), lessonMark.getDateOfMaking()));
+        for (Object[] lessonMark : lessonMarks) {
+            lessonMarksDTOs.add(new LessonMarksDTO((Integer) lessonMark[0], (Float) lessonMark[1], (Short) lessonMark[2], (Date) lessonMark[3]));
         }
-
         return lessonMarksDTOs;
     }
 }
